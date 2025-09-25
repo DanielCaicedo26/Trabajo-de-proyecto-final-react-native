acefbf6ad6702911871978545f706591538ec1e5
Daniel <Daniel3204155185@gmail.com>
2025-09-25 07:32:22 -0500

    Daniel config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 
 // Archivo de configuración centralizada para las URLs base de la API
 // Modifica aquí la URL del backend según el entorno (desarrollo, producción, etc.)
-export const API_HOST = 'http://192.168.1.5:5162';
+export const API_HOST = 'http://192.168.1.3:5162';
