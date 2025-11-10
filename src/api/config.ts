2dbf8354c21c05061a5c7e4d9403254696f03b92
Daniel <Daniel3204155185@gmail.com>
2025-11-10 14:53:06 -0500

    ya funciona por fin

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,7 +4,7 @@
  */
 
 // Valor por defecto si no hay variable de entorno
-const DEFAULT_API_HOST = 'http://192.168.1.6:7286';
+const DEFAULT_API_HOST = "http://172.30.1.90:7285";
 const DEFAULT_API_TIMEOUT = 30000;
 
 // Intentar leer de variables de entorno
